6f41485bba5cc10946313ea71dda657cf676f11b
riscoriscodev <ricomcfilho@gmail.com>
2025-06-26 19:59:31 -0300

    edit

--- mlp.py
+++ mlp.py
@@ -20,11 +20,8 @@ skf = StratifiedKFold(n_splits=k, shuffle=True, random_state=42)
 
 accuracies = []
 
-# Will be used to plot the ROC curve of the test with better accuracy.
-best_acc = 0
-best_y_test = None
-best_y_proba = None
-best_y_train = None
+# Will be used to analyze the accuracy and plot the ROC curve later.
+fold_data = []
 
 fold = 1
 
@@ -47,40 +44,48 @@ for train_index, test_index in skf.split(X, y):
     y_pred = mlp.predict(X_test)
     acc = accuracy_score(y_test, y_pred)
     print(f"Accuracy: {acc: .4f}")
-    #print(classification_report(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
     accuracies.append(acc)
 
-    # Save the test with the best score:
-    if acc > best_acc:
-        best_acc = acc
-        best_y_test = y_test
-        best_y_proba = mlp.predict_proba(X_test) # Get the probabilites to plot the ROC curve.
-        best_y_train = y_train
-
+    # Save the data necessary to plot the ROC curve
+    fold_data.append({
+        'accuracy': acc,
+        'y_test': y_test,
+        'y_proba': mlp.predict_proba(X_test), # Get the probabilites
+        'y_train': y_train
+    })
 
     fold += 1
 
-print(f"Average accuracy after {k} folds: {np.mean(accuracies): .4f}")
-print(f"Best accuracy between folds: {best_acc:.4f}")
+mean_acc = np.mean([f['accuracy'] for f in fold_data])
+print(f"Average accuracy after {k} folds: {mean_acc: .4f}")
+
+# Gets the fold with the closest accuracy to the mean
+mean_ant = max(
+    [f for f in fold_data if f['accuracy'] <= mean_acc],
+    key=lambda x: x['accuracy']
+)
+
+print(f"Closest accuracy to the average: {mean_ant['accuracy']: .4f}")
 
 
 # ROC Curve using OvR macro-average
 # Calcula a curva ROC pra cada classe e depois tira a média)
 
 # Necessary to plot
-label_binarizer = LabelBinarizer().fit(best_y_train)
-y_onehot_test = label_binarizer.transform(best_y_test)
+label_binarizer = LabelBinarizer().fit(mean_ant['y_train'])
+y_onehot_test = label_binarizer.transform(mean_ant['y_test'])
 
 macro_roc_auc_ovr = roc_auc_score(
-    best_y_test,
-    best_y_proba,
+    mean_ant['y_test'],
+    mean_ant['y_proba'],
     multi_class="ovr",
     average="macro",
 )
 
 RocCurveDisplay.from_predictions(
         y_onehot_test.ravel(),
-        best_y_proba.ravel(),
+        mean_ant['y_proba'].ravel(),
         name=f"Macro-average ROC",
         plot_chance_level=True,
         curve_kwargs={"color": "red"},
@@ -89,4 +94,4 @@ RocCurveDisplay.from_predictions(
 plt.savefig("mlp_macro_roc.png")
 plt.close()
 
-print(f"Macro-averaged One-vs-Rest ROC AUC score of the best fold:\n{macro_roc_auc_ovr:.4f}")
+print(f"Macro-averaged One-vs-Rest ROC AUC score:\n{macro_roc_auc_ovr:.4f}")
